b74579bccdbf61aff4d3abb80ec844f336de4b96
kirtansoni1 <kirtansoni001@gmail.com>
2024-06-25 18:37:04 +0200

    - adding all the files - updating readme

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # CrowdAnalysisSetupApp
 Setup Application for CrowdAnalysis Project
+
+# Overview
+The Crowd Analysis Setup App is designed to facilitate the setup of Nvidia Xavier NX device using either a USB or WiFi wireless connection. This app ensures that the device is properly configured and connected, making it easy to run and manage the main crowd analysis server on the Nvidia Xavier NX hardware.
+
+# Features
+- Nvidia Device Setup: The app allows users to set up Nvidia devices via USB or WiFi.
+- USB Connection Check: The app checks if the device is connected via USB using Nvidia's Vendor ID (VID).
+- WiFi Network Connection: If the device is not already set up, users can connect to a wireless network, and the app will automatically display the IP address of the Crowd Analysis server.
+- Crowd Management System: A button is provided to start the crowd management system as a service on the device if it is connected.
+- Service Routine: A dedicated service routine is added for running the crowd analysis server.
+- Stop Button: A stop button is included to stop the service running in the background.
+
+# Service Configuration
+To ensure the crowd analysis server runs smoothly, a systemd service has been created. Below is the configuration for crowdanalysis.service.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+[Unit]
+Description=Crowd Analysis Flask Server
+After=network.target
+
+[Service]
+User=dit
+WorkingDirectory=/home/dit/kirtanSoni_master_thesis/CrowdAnalysis
+ExecStart=/home/dit/kirtanSoni_master_thesis/CrowdAnalysis/.venv/bin/python /home/dit/kirtanSoni_master_thesis/CrowdAnalysis/main.py
+Restart=always
+Environment="PATH=/home/dit/kirtanSoni_master_thesis/CrowdAnalysis/.venv/bin"
+
+[Install]
+WantedBy=multi-user.target
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# How to run and use the software:
+If you want to setup the code base and run it over an IDE like VScode, follow the below steps given in "How to setup the python enviornmnet", but if you want to directly start using the software then follow the steps given in "How to open the software from binary"
+IMPORTANT: This software only works with Linux OS
+
+## How to setup the python enviornmnet:
+- If Linux:
+    - python3 -m venv .venv
+    - source .venv/bin/activate
+    - pip install --upgrade pip setuptools
+    - pip install -r requirements.txt
+    - Run the software in IDE:
+        - python3 ./app.py
+
+## How to open the software from binary:
+- If Linux:
+    - Double click the ./dist/CrowdAnalysis
+    - OR
+    - Open a Terminal inside the CrowdAnalysisSetupApp root directory
+    - Enter the following cmd:
+        - ./dist/CrowdAnalysis
+    - The software will open
+
+## How to use this software:
+- After opening the software, it will look for the device via USB or WiFi
+- If you are setting up the device first time connect the device to the laptop with USB cable and the software will automatically detect the device.
+- After detection, you can enter the desired WiFi credentials and click "Connect to WiFi" button
+- If the device is connected to the WiFi it will show "successfully connected", if not make sure the credentials are right and try again.
+- Once connected to the WiFi, you can disconnect the USB and the software will automatically connect to it wirelessly via WiFi.
+- Now, you can start the serivce by pressing "Start Crowd Analysis Server" button and wait for confiramtion message.
+- Once confirmed, use the IP address displayed, copy it and enter on any browser to connect to the Main GUI
+
---
+++ app.py
@@ -0,0 +1,285 @@
+from flask import Flask, render_template, jsonify, request
+import subprocess
+import paramiko
+import platform
+import threading
+import webview
+import os
+import json
+import time
+import signal
+
+app = Flask(__name__)
+
+# Define the Nvidia Vendor ID
+NVIDIA_VID = '0955'
+
+browser_instance = None
+flask_thread = None
+
+ssh_host = '192.168.55.1'
+ssh_user = 'dit'
+ssh_password = 'dit'
+ip_file_path = 'ip.json'
+flask_port = 5100
+
+
+def check_usb_connection():
+    try:
+        if platform.system() == "Windows":
+            result = subprocess.run(['wmic', 'path', 'Win32_USBControllerDevice',
+                                    'get', 'Dependent'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            return NVIDIA_VID in output
+        else:
+            result = subprocess.run(['lsusb'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            return NVIDIA_VID in output
+    except Exception as e:
+        return False
+
+
+def get_ip_address():
+    try:
+        with open(ip_file_path, 'r') as file:
+            cur_ip = json.load(file)
+
+        # Create an SSH client
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if not check_usb_connection():
+            ssh.connect(cur_ip['ip_addr'], username=ssh_user, password=ssh_password, timeout=5)
+        else:
+            ssh.connect(ssh_host, username=ssh_user, password=ssh_password, timeout=5)
+
+        # Execute the command to get IP address of wlan0
+        command = "ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        ip_address = stdout.read().decode().strip()
+        ssh.close()
+
+        # Update the IP address in the file if it has changed
+        if cur_ip['ip_addr'] != ip_address:
+            with open(ip_file_path, 'w') as file:
+                new_ip = {'ip_addr': ip_address}
+                json.dump(new_ip, file)
+
+        return ip_address if ip_address else "Unknown"
+    except Exception as e:
+        return "Unknown"
+
+
+def is_wifi_connected(usb_status):
+    try:
+        # Read the current IP address from the file
+        with open(ip_file_path, 'r') as file:
+            cur_ip = json.load(file)
+
+        # Create an SSH client
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if not usb_status:
+            ssh.connect(cur_ip['ip_addr'], username=ssh_user, password=ssh_password, timeout=5)
+        else:
+            ssh.connect(ssh_host, username=ssh_user, password=ssh_password, timeout=5)
+
+        time.sleep(2)
+
+        # Execute the command to check WiFi connection
+        command = "nmcli -t -f DEVICE,TYPE,STATE,CONNECTION device"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode().strip()
+        ssh.close()
+
+        for line in output.split('\n'):
+            parts = line.split(':')
+            if len(parts) >= 4 and parts[1] == 'wifi' and parts[2] == 'connected':
+                return True, parts[3]  # return connection name
+        return False, ''
+    except Exception as e:
+        return False, ''
+
+
+def execute_ssh_command(command):
+    try:
+        # Read the current IP address from the file
+        with open(ip_file_path, 'r') as file:
+            cur_ip = json.load(file)
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if not check_usb_connection():
+            ssh.connect(cur_ip['ip_addr'], username=ssh_user, password=ssh_password, timeout=5)
+        else:
+            ssh.connect(ssh_host, username=ssh_user, password=ssh_password, timeout=5)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        ssh.close()
+        return output, error
+    except Exception as e:
+        return "", str(e)
+
+
+def check_and_kill_port(port):
+    try:
+        result = subprocess.run(['lsof', '-i', f':{port}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode().strip()
+        if output:
+            lines = output.split('\n')
+            for line in lines[1:]:
+                if line:
+                    parts = line.split()
+                    pid = int(parts[1])
+                    os.kill(pid, signal.SIGKILL)
+            print(f"Port {port} is now free.")
+            time.sleep(5)
+        else:
+            print(f"Port {port} is not occupied.")
+    except Exception as e:
+        print(f"Error checking/killing port {port}: {e}")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_ip', methods=['POST'])
+def get_ip():
+    ip_address = get_ip_address()
+    return jsonify({'ip_address': ip_address})
+
+
+@app.route('/connect_wifi', methods=['POST'])
+def connect_wifi():
+    data = request.json
+    ssid = data.get('ssid')
+    password = data.get('password')
+
+    if ssid and password:
+        try:
+            if not check_usb_connection():
+                return jsonify({'message': f"First connect the device to the host with USB."}), 500
+
+            # Create an SSH client
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(ssh_host, username=ssh_user, password=ssh_password, timeout=5)
+
+            # Check if there is an active connection on wlan0
+            wifi_status, _ = is_wifi_connected(usb_status=True)
+
+            if wifi_status:
+                # Disconnect the active connection on wlan0
+                disconnect_command = f"echo {ssh_password} | sudo -S nmcli d disconnect wlan0"
+                stdin, stdout, stderr = ssh.exec_command(disconnect_command)
+                disconnect_output = stdout.read().decode().strip()
+                disconnect_error = stderr.read().decode().strip()
+
+                if disconnect_error:
+                    return jsonify({'message': f"Failed to disconnect from current WiFi: {disconnect_error}"}), 500
+
+            # Construct the nmcli command to connect to the new WiFi network with sudo
+            connect_command = f"echo {ssh_password} | sudo -S nmcli d wifi connect '{ssid}' password '{password}'"
+            stdin, stdout, stderr = ssh.exec_command(connect_command)
+
+            # Read the output and error streams
+            connect_output = stdout.read().decode().strip()
+            connect_error = stderr.read().decode().strip()
+
+            ssh.close()
+
+            if connect_error:
+                return jsonify({'message': f"Failed to connect to WiFi: {connect_error}"}), 500
+            else:
+                return jsonify({'message': 'Connected to WiFi successfully!', 'output': connect_output})
+
+        except Exception as e:
+            return jsonify({'message': f"Failed to connect to WiFi: {str(e)}"}), 500
+    else:
+        return jsonify({'message': 'Please enter both SSID and Password'}), 400
+
+
+@app.route('/check_status')
+def check_status():
+    usb_status = check_usb_connection()
+    wifi_status, wifi_name = is_wifi_connected(usb_status)
+    return jsonify({'usb_connected': usb_status, 'wifi_connected': wifi_status, 'wifi_name': wifi_name})
+
+
+@app.route('/start_server', methods=['POST'])
+def start_server():
+    start_service_command = "sudo systemctl start crowdanalysis.service"
+    output, error = execute_ssh_command(start_service_command)
+
+    # Add a delay to allow the server to start
+    time.sleep(5)
+
+    # Check if the server is running
+    check_server_command = "systemctl is-active crowdanalysis.service"
+    check_output, check_error = execute_ssh_command(check_server_command)
+
+    if 'active' in check_output.strip():
+        return jsonify({'message': f'Crowd Analysis Server has started, please use the above IP to access the GUI.', 'output': output})
+    else:
+        return jsonify({'message': "Failed to start server: The server service is not active.", 'output': check_error}), 500
+
+
+@app.route('/stop_server', methods=['POST'])
+def stop_server():
+    stop_service_command = "sudo systemctl stop crowdanalysis.service"
+    output, error = execute_ssh_command(stop_service_command)
+    if error:
+        return jsonify({'message': f"Failed to stop Crowd Analysis Server: {error}"}), 500
+    else:
+        return jsonify({'message': 'Crowd Analysis Server stopped successfully', 'output': output})
+
+
+@app.route('/server_status', methods=['GET'])
+def server_status():
+    check_server_command = "systemctl is-active crowdanalysis.service"
+    output, error = execute_ssh_command(check_server_command)
+    if 'active' in output.strip() and not 'inactive' in output.strip():
+        return jsonify({'status': 'running'})
+    else:
+        return jsonify({'status': 'stopped'})
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    global browser_instance
+    if browser_instance:
+        browser_instance.destroy()
+    # Terminate the Flask server and the application
+    threading.Timer(1, lambda: os._exit(0)).start()
+    return jsonify({'message': 'Shutting Down...'})
+
+
+def start_flask():
+    app.run(debug=False, use_reloader=False, port=flask_port)
+
+
+if __name__ == '__main__':
+    # Making ip.json file to store device ip, Check if the file exists, if not create it with default data
+    try:
+        with open(ip_file_path, 'r') as file:
+            data = json.load(file)
+    except:
+        with open(ip_file_path, 'w') as file:
+            init_data = {'ip_addr': ssh_host}
+            json.dump(init_data, file)
+
+    # Check and kill the process occupying the port before starting Flask
+    check_and_kill_port(flask_port)
+
+    # Start Flask in a separate thread
+    flask_thread = threading.Thread(target=start_flask)
+    flask_thread.start()
+
+    time.sleep(2)
+
+    # Start the webview after Flask has started
+    browser_instance = webview.create_window("Crowd Analysis Sytem Setup",
+                                             "http://127.0.0.1:5100", fullscreen=True)
+    webview.start()
